7212545e71057a38c71603fe85c03cb15177ea8a
Sergey_Sonne <berit2008@mail.ru>
2024-11-15 00:44:26 +0300

    Добавлены изменения

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;         // Имя
+    public String surname;      // Фамилия
+    public String patronymic;   // Отчество
+    public String phone;        // Номер телефона
+    public String passport;     // Паспортные данные
+    public boolean subscription; // Согласие на получение новостей о скидках
+    public FormDate birthday;   // Дата рождения (объект класса FormDate)
+}
